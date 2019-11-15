46d9c41088d73300be72c64f8008dc2aba912d58
filigreti <aolfiligre@gmail.com>
2019-11-15 15:50:10 +0100

    WIP

--- html/assets/js/app.js
+++ html/assets/js/app.js
@@ -1,1 +1,11 @@
-console.log("we out here three");
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    nav: true,
+    dots: false,
+    margin: 10,
+    autoWidth: true,
+    center: true,
+    loop: true,
+    items: 1
+  });
+});
